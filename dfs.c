f9f9b847807f906d27a85ead4855eae1dcdc8a36
Janscode <55261974+Janscode@users.noreply.github.com>
2020-12-10 14:18:45 -0700

    fill in details for serve, and spawn new threads

--- dfs.c
+++ dfs.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <sys/socket.h>
 #include <arpa/inet.h>
+#include <sys/time.h>
+#include <pthread.h>
 //todo: import standard io, networking, and multithreading libraries
 
 //todo: write thread to serve get request
@@ -24,18 +26,49 @@
 
 //todo: write main thread routine to serve all requests
 void serve(void * connection){
+    int sock_fd, bytes;
+    sock_fd = *(int *) connection;
+    free(connection);
+    char buf[1028];
+    char username[45];
+    char password[45];
+    char command[4];
+    char filename[40];
+    
+    bytes = recv(sock_fd, buf, 1028, 0); //practice: limit bytes read so that the string won't overflow the buffers
+    if (bytes < 0){
+        perror("recv failed");
+    }
+    //parse socket stream for request type, username, password
+    sscanf(buf, "%s %s %s %s", username, password, command, filename); //practice: deal with filenames with spaces
+    if (0){
+        //todo: check username and password
+            //todo: if username is valid but no folder exists, create folder
+            //todo: if username is invalid, send back error signal
+    }
+    else{
+        //todo: call appropriate handler, passing request details
+        if (!strncmp(command, "put", 3)){
+            
+        }
+        else if (!strncmp(command, "get", 3)){
+            
+        }
+        else if (!strncmp(command, "list", 4)){   
 
+        }
+    }
+    
 }
-    //todo: parse socket stream for request type, username, password
-    //todo: check username and password
-        //todo: if username is valid but no folder exists, create folder
-        //todo: if username is invalid, send back error signal
-    //todo: call appropriate handler, passing request details
 
 //todo: write main
 int main(int argc, char ** argv){
-    int sock_fd, port, connection_fd;
+    int sock_fd, port, ready;
+    int *  connection;
     struct sockaddr_in server_addr;
+    pthread_t tid;
+    fd_set sock_set;
+    struct  timeval timeout;
     //todo: read config file
     port = 8080;
         //todo: create username password data structure (linked list?) alternativley, just parse through config file each time
@@ -51,7 +84,7 @@ int main(int argc, char ** argv){
     server_addr.sin_addr.s_addr = INADDR_ANY;
     server_addr.sin_port = htons(port);
     //bind socket to port number
-    if (bind(sock_fd, &server_addr, sizeof(server_addr)) < 0){
+    if (bind(sock_fd, (struct sockaddr *) &server_addr, sizeof(server_addr)) < 0){
         perror("bind failed");
     }
     //put socket in passive mode with allowable backlog of 4 connections
@@ -60,6 +93,22 @@ int main(int argc, char ** argv){
     }
     //todo: while loop to listen to socket using select
     while(1){
+        timeout.tv_sec = 1;
+        timeout.tv_usec = 0;
+        FD_ZERO(&sock_set);
+        FD_SET(sock_fd, &sock_set);
+        ready = select(sock_fd + 1, &sock_set, NULL, NULL, &timeout);
+        if (ready < 0){
+            perror("select failed");
+        }
+        else if (ready){
+            connection = malloc(sizeof(int));
+            if ((*connection = accept(sock_fd, &server_addr, sizeof(server_addr))) < 0){
+                perror("accept failed");
+            }
+            pthread_create(&tid, NULL, serve, (void *)connection);
+            pthread_detach(tid);
+        }
          //todo: accept connection and spawn new thread
     }
     
