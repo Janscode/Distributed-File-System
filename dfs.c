85b55c1fa316efcf8de49fdad7a1908fe00d6e44
Janscode <55261974+Janscode@users.noreply.github.com>
2020-12-10 12:54:57 -0700

    Add code to create socket in main

--- dfs.c
+++ dfs.c
@@ -1,3 +1,6 @@
+#include <stdio.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
 //todo: import standard io, networking, and multithreading libraries
 
 //todo: write thread to serve get request
@@ -20,6 +23,9 @@
 
 
 //todo: write main thread routine to serve all requests
+void serve(void * connection){
+
+}
     //todo: parse socket stream for request type, username, password
     //todo: check username and password
         //todo: if username is valid but no folder exists, create folder
@@ -27,10 +33,38 @@
     //todo: call appropriate handler, passing request details
 
 //todo: write main
+int main(int argc, char ** argv){
+    int sock_fd, port, connection_fd;
+    struct sockaddr_in server_addr;
     //todo: read config file
+    port = 8080;
         //todo: create username password data structure (linked list?) alternativley, just parse through config file each time
             //practice: hash and salt passwords
             //practice: implement a more efficient username lookup
-        //todo: open socket on correct port
+    //create socket
+    if ((sock_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0){
+        perror("socket failed");
+    }
+    //practice: use setsockopt to make program more robust
+    //build server address
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_addr.s_addr = INADDR_ANY;
+    server_addr.sin_port = htons(port);
+    //bind socket to port number
+    if (bind(sock_fd, &server_addr, len(server_addr)) < 0){
+        perror("bind failed");
+    }
+    //put socket in passive mode with allowable backlog of 4 connections
+    if (listen(sock_fd, 4) < 0){
+        perror("listen failed");
+    }
     //todo: while loop to listen to socket using select
-        //todo: accept connection and spawn new thread
+    while(1){
+         //todo: accept connection and spawn new thread
+    }
+    
+    
+       
+}
+    
+    
